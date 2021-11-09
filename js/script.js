94e9ae91ad79b6445b98dcf90ea1e5e2dbae6c17
SimoneSpirito <simospiri01@gmail.com>
2021-11-09 16:34:24 +0100

    fizzbuzz

--- js/script.js
+++ js/script.js
@@ -2,7 +2,23 @@
 const row = document.querySelector('.row');
 
 for (let i = 1; i <= 100; i++){
-    const box = `<div class="box">${i}</div>`;
-    row.innerHTML += box;
-
+    
+    if (i % 3 == 0){
+        if (i % 5 == 0){
+            const box = `<div class="box red">fizzbuzz</div>`;
+            row.innerHTML += box;
+        }
+        else {
+            const box = `<div class="box green">fizz</div>`;
+            row.innerHTML += box;
+        }
+    }
+    else if (i % 5 == 0){
+        const box = `<div class="box yellow">buzz</div>`;
+        row.innerHTML += box;
+    }
+    else {
+        const box = `<div class="box">${i}</div>`;
+        row.innerHTML += box;
+    }
 }
